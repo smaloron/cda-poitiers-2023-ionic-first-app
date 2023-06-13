abf241cde22c582f04f391da1a8937858da874d8
seb <contact@smaloron.eu>
2023-06-13 10:33:20 +0200

    saisie formulaire

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -7,17 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPage implements OnInit {
 
-  public name = "Niels";
-  age = 89;
-  buttonColor = "primary"
+  person = {
+    name: 'Brahé',
+    firstName: 'Tycho',
+    profession: '',
+    skills: [],
+    bio: '',
+    gender: 'F',
+    age: 30
+  }
+
+  buttonColor = "primary";
+  hiddenForm = true;
+  formIsValid = false;
+  formIsTouched = false;
 
   constructor() { }
 
   getOlder() {
-    this.age++;
-    if (this.age >= 100) {
+    this.person.age++;
+    if (this.person.age >= 100) {
       this.buttonColor = "danger";
     }
+  }
+
+  validatePerson() {
+
+    if (this.person.firstName === '') {
+      return false;
+    }
+    if (this.person.name === '') {
+      return false;
+    }
+    if (this.person.bio === '') {
+      return false;
+    }
+    if (this.person.profession === '') {
+      return false;
+    }
+    if (this.person.age < 20 || this.person.age > 80) {
+      return false;
+    }
+
+    return true;
+  }
+
+  onSubmit() {
+    this.formIsTouched = true;
+    this.formIsValid = this.validatePerson();
   }
 
   ngOnInit() {
